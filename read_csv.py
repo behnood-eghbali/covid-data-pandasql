1a362e1fa6d0d97cda02e0557457f9f3fd5ec0ca
Behnood Eghbali <behnood-eghbali@users.noreply.github.com>
2021-01-03 19:51:40 +0330

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -8,9 +8,20 @@ Created on Thu Nov 26 22:10:21 2020
 import os
 import pandas as pd
 import pandasql as pds
+import matplotlib.pyplot as plt
 
 data_path = os.path.expanduser('~/national-history.csv')
 
 df = pd.read_csv(data_path)
 
-df2 = pds.sqldf("""SELECT COUNT(1), states FROM df GROUP BY death""")
+df2 = pds.sqldf(""" SELECT positive FROM df GROUP BY strftime('%m', date) """)
+
+df3 = pds.sqldf(""" SELECT negative FROM df GROUP BY strftime('%m', date) """)
+
+df4 = pds.sqldf(""" SELECT death FROM df GROUP BY strftime('%m', date) """)
+
+plt.figure()
+
+df2.plot.area()
+df3.plot.area()
+df4.plot.area()
